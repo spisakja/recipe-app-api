2033e961da9e419c6e4cd6e5d1244840d314755f
spisakja <jspisak@spiru.cz>
2022-06-24 15:56:19 +0200

    085 - Implement create recipe API

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -112,7 +112,7 @@ class PrivateRecipeApiTests(TestCase):
         res = self.client.post(RECIPES_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        recipe = Recipe.objects.get(res.data['id'])
-        for k, v in payload.item():
+        recipe = Recipe.objects.get(pk=res.data['id'])
+        for k, v in payload.items():
             self.assertEqual(getattr(recipe, k), v)
         self.assertEqual(recipe.user, self.user)
