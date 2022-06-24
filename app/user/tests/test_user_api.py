14d4c2985824264703fdfc268813d2ef4e8d660f
spisakja <jspisak@spiru.cz>
2022-06-24 08:58:13 +0200

    070 - Implement token API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -80,7 +80,7 @@ class PublicUserApiTests(TestCase):
         }
         res = self.client.post(TOKEN_URL, payload)
 
-        self.assertIn('toke', res.data)
+        self.assertIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_create_token_bad_credentials(self):
