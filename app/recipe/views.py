743408938840a2aac456a86f584c3d0b174b7be5
spisakja <jspisak@spiru.cz>
2022-06-30 13:27:16 +0200

    109 - Implement update ingredient API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -54,7 +54,9 @@ class TagViewSet(mixins.DestroyModelMixin,
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
 
-class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class IngredientViewSet(mixins.UpdateModelMixin,
+                        mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
     """View for manage ingredient APIs."""
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
